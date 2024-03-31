2f044e2029f1de846d705a4c910b1f4676a3e76e
seungriyou <seungri0826@gmail.com>
2024-03-31 14:07:17 +0900

    feat: add custom filter for obfuscating emails (#5)

--- socialapi/logging_conf.py
+++ socialapi/logging_conf.py
@@ -1,6 +1,44 @@
+import logging
 from logging.config import dictConfig
 
 from socialapi.config import DevConfig, config
+
+
+# custom filter for hiding emails
+def obfuscated(email: str, obfuscated_length: int) -> str:
+    characters = email[:obfuscated_length]
+    first, last = email.split("@")
+    return characters + ("*" * (len(first) - obfuscated_length)) + "@" + last
+
+
+class EmailObfuscationFilter(logging.Filter):
+    """
+    for not logging some personal data(ex. emails) which are included in extra argument
+    extra argument:
+        ```
+        logger.debug(query, extra={"email": "bob@example.net"})
+        ```
+        -> to log extra information that isn't included in formatting message
+    """
+
+    def __init__(self, name: str = "", obfuscated_length: int = 2) -> None:
+        super().__init(name)
+        self.obfuscated_length = obfuscated_length
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        """
+        If return true, the log record will be displayed.
+        Otherwise, the log record will be filtered out.
+
+        If you want to add new variables like "correlation_id", just set an attribute of record.
+        ```
+        record.your_variable = "123"
+        ```
+        This variable can be used in formatters.
+        """
+        if "email" in record.__dict__:
+            record.email = obfuscated(record.email, self.obfuscated_length)
+        return True
 
 
 def configure_logging() -> None:
@@ -14,7 +52,12 @@ def configure_logging() -> None:
                     "()": "asgi_correlation_id.CorrelationIdFilter",
                     "uuid_length": 8 if isinstance(config, DevConfig) else 32,
                     "default_value": "-",
-                }
+                },
+                # add custom filter
+                "email_obfuscation": {
+                    "()": EmailObfuscationFilter,  # class -> use class // str -> import
+                    "obfuscated_length": 2 if isinstance(config, DevConfig) else 0,
+                },
             },
             "formatters": {
                 "console": {
@@ -37,7 +80,7 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",  # -- change to rich handler
                     "level": "DEBUG",
                     "formatter": "console",  # -- one of the "formatters"
-                    "filters": ["correlation_id"],
+                    "filters": ["correlation_id", "email_obfuscation"],
                 },
                 # when file size reaches certain size...
                 "rotating_file": {
@@ -49,7 +92,7 @@ def configure_logging() -> None:
                     "maxBytes": 1024 * 1024,  # 1MB per file
                     "backupCount": 5,  # 5 files
                     "encoding": "utf8",
-                    "filters": ["correlation_id"],
+                    "filters": ["correlation_id", "email_obfuscation"],
                 },
             },
             "loggers": {
